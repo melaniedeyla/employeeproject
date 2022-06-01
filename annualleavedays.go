d612819c0901be8be12899aa8b9d2c7730f0febe
melaniedeyla <72774998+melaniedeyla@users.noreply.github.com>
2022-06-01 16:05:45 +1000

    Update annualleavedays.go

--- annualleavedays.go
+++ annualleavedays.go
@@ -1,10 +1,13 @@
-package person
+package leave_go
+
+import (
+	person "secondProject/person"
+)
 
 type AnnualLeaveDays struct{}
 
-func (al AnnualLeaveDays) Leave(e *person.Employee) bool {
+func (al AnnualLeaveDays) LeavesTaken(e *person.Employee) bool {
 	//Annual Leave left for employee
 	//Remember to do the subtraction from annualleave days to leavetaken
 	return true
 }
-
